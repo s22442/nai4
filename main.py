e80a14696dee052067de12934571ad125e612867
brofrain <knwelc@gmail.com>
2023-11-26 12:25:16 +0100

    feat: sonar svm

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report
+from sklearn import svm
 
 sonar = np.genfromtxt('sonar.csv', delimiter=',', dtype=str)
 
@@ -20,8 +21,22 @@ sonar_y_test_dc_pred = sonar_dc_classifier.predict(sonar_X_test)
 print("Sonar | Decision Tree | Training Report")
 print(classification_report(sonar_y_train,
       sonar_dc_classifier.predict(sonar_X_train)))
-
 print()
 
 print("Sonar | Decision Tree | Testing Report")
 print(classification_report(sonar_y_test, sonar_y_test_dc_pred))
+print()
+
+sonar_svm_classifier = svm.SVC()
+sonar_svm_classifier.fit(sonar_X_train, sonar_y_train)
+
+sonar_y_test_svm_pred = sonar_svm_classifier.predict(sonar_X_test)
+
+print("Sonar | SVM | Training Report")
+print(classification_report(sonar_y_train,
+      sonar_svm_classifier.predict(sonar_X_train)))
+print()
+
+print("Sonar | SVM | Testing Report")
+print(classification_report(sonar_y_test, sonar_y_test_svm_pred))
+print()
