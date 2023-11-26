5f0fcabc08d86a0699ff6e50ce6831f761b58f14
brofrain <knwelc@gmail.com>
2023-11-26 11:53:52 +0100

    feat: sonar decision tree

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from ucimlrepo import fetch_ucirepo
-import csv
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
 
-mushroom = fetch_ucirepo(id=73)
+sonar = np.genfromtxt('sonar.csv', delimiter=',', dtype=str)
 
-mushroom_X = mushroom.data.features
-mushroom_y = mushroom.data.targets
+sonar_X = sonar[:, :-1].astype(float)
+sonar_y = sonar[:, -1]
 
-print(mushroom.metadata)
-# print(mushroom.variables)
+sonar_X_train, sonar_X_test, sonar_y_train, sonar_y_test = train_test_split(
+    sonar_X, sonar_y, test_size=0.5)
 
-with open('sonar.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    sonar = []
-    for row in reader:
-        sonar.append(row)
 
-print(sonar)
+sonar_dc_classifier = DecisionTreeClassifier(random_state=0, max_depth=8)
+sonar_dc_classifier.fit(sonar_X_train, sonar_y_train)
+
+sonar_y_test_dc_pred = sonar_dc_classifier.predict(sonar_X_test)
+
+print("Sonar | Decision Tree | Training Report")
+print(classification_report(sonar_y_train,
+      sonar_dc_classifier.predict(sonar_X_train)))
+
+print()
+
+print("Sonar | Decision Tree | Testing Report")
+print(classification_report(sonar_y_test, sonar_y_test_dc_pred))
